49e5e02f5b6399877614a90d8e96fe4766f74888
Anna Zubchenko <annazubchenko282@gmail.com>
2024-06-02 14:40:10 +0300

    renamed files

--- src/pages/homePage/styles.ts
+++ src/pages/homePage/styles.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Section } from 'src/components/section';
+import { Theme } from 'src/styles/theme';
 
-export const HomeSection = styled(Section)`
-  background-color: aqua;
+export const HomeSection = styled(Section)<{ theme?: Theme }>`
+  background-color: ${({ theme }) => theme.colors.error_red};
 `;
